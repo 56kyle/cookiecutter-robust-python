c3aca2a842450afffbbbcf4f8664b8e8a416c9a3
Kyle Oliver <oliverke@mail.uc.edu>
2025-05-07 22:37:56 -0400

    feat: update sync-uv-with-demo.py to use typer and uv

--- scripts/sync-uv-with-demo.py
+++ scripts/sync-uv-with-demo.py
@@ -1,50 +1,60 @@
-"""Module used to sync the poetry.lock file with one generated using a demo project."""
+"""Module used to sync the uv.lock file with one generated using a demo project."""
 
 import shutil
 import sys
+from functools import partial
 from pathlib import Path
 
-import click
+import typer
+
+from typer.models import OptionInfo
 
 from loguru import logger
 
 
-FOLDER_TYPE: click.Path = click.Path(dir_okay=True, file_okay=False, resolve_path=True, path_type=Path)
+FolderOption: partial[OptionInfo] = partial(
+    typer.Option, dir_okay=True, file_okay=False, resolve_path=True, path_type=Path
+)
 
 
-def sync_poetry_with_demo(template_folder: Path, demos_cache_folder: Path, demo_name: str) -> None:
+def sync_uv_with_demo(template_folder: Path, demos_cache_folder: Path, demo_name: str) -> None:
     demo_root: Path = demos_cache_folder / demo_name
-    demo_poetry_lock_path: Path = _find_poetry_lock_path(demo_root)
-    output_poetry_lock_path: Path = _find_poetry_lock_path(template_folder)
+    demo_uv_lock_path: Path = _find_uv_lock_path(demo_root)
+    output_uv_lock_path: Path = _find_uv_lock_path(template_folder)
 
-    _copy_poetry_lock_from_demo(
-        demo_poetry_lock_path=demo_poetry_lock_path, output_poetry_lock_path=output_poetry_lock_path
+    _copy_uv_lock_from_demo(
+        demo_uv_lock_path=demo_uv_lock_path, output_uv_lock_path=output_uv_lock_path
     )
-    logger.info(f"Copied demo from {demo_poetry_lock_path=} to {output_poetry_lock_path=}.")
+    logger.info(f"Copied demo from {demo_uv_lock_path=} to {output_uv_lock_path=}.")
 
 
-def _copy_poetry_lock_from_demo(demo_poetry_lock_path: Path, output_poetry_lock_path: Path) -> None:
-    """Copies over the poetry.lock file from the provided demo project root."""
+
+def _copy_uv_lock_from_demo(demo_uv_lock_path: Path, output_uv_lock_path: Path) -> None:
+    """Copies over the uv.lock file from the provided demo project root."""
     shutil.copy(
-        src=demo_poetry_lock_path,
-        dst=output_poetry_lock_path,
+        src=demo_uv_lock_path,
+        dst=output_uv_lock_path,
     )
 
 
-def _find_poetry_lock_path(search_root: Path) -> Path:
-    for path in search_root.rglob("poetry.lock"):
+def _find_uv_lock_path(search_root: Path) -> Path:
+    for path in search_root.rglob("uv.lock"):
         return path
-    raise FileNotFoundError(f"Failed to find a poetry.lock within the provided search path: {search_root=}")
+    raise FileNotFoundError(f"Failed to find a uv.lock within the provided search path: {search_root=}")
 
 
-@click.command()
-@click.option("--template-folder", "-t", required=True, type=FOLDER_TYPE)
-@click.option("--demos-cache-folder", "-c", required=True, type=FOLDER_TYPE)
-@click.option("--demo-name", "-d", required=True, type=str)
-def main(template_folder: Path, demos_cache_folder: Path, demo_name: str) -> None:
-    """Updates the poetry.lock file."""
+cli: typer.Typer = typer.Typer()
+
+
+@cli.callback(invoke_without_command=True)
+def main(
+    template_folder: Annotated[Path, FolderOption("--template-folder", "-t", exist=True)],
+    demos_cache_folder: Annotated[Path, FolderOption("--demos-cache-folder", "-c", exist=True)],
+    demo_name: Annotated[str, typer.Option("--demo-name", "-d")]
+) -> None:
+    """Updates the uv.lock file."""
     try:
-        sync_poetry_with_demo(
+        sync_uv_with_demo(
             template_folder=template_folder, demos_cache_folder=demos_cache_folder, demo_name=demo_name
         )
     except Exception as error:
@@ -53,4 +63,4 @@ def main(template_folder: Path, demos_cache_folder: Path, demo_name: str) -> Non
 
 
 if __name__ == "__main__":
-    main(prog_name="prepare-github-release")
+    cli()
