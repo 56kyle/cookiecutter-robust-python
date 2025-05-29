481418d706d2fe1bdd2b1b3b5a8395bcd18db2c4
Kyle Oliver <oliverke@mail.uc.edu>
2025-05-28 20:03:31 -0400

    feat: expand and refactor integration tests for the template

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,7 @@
 """Fixtures used in all tests for cookiecutter-robust-python."""
 
-import os
 import subprocess
 from pathlib import Path
-from typing import Generator
 
 import pytest
 from _pytest.tmpdir import TempPathFactory
@@ -16,49 +14,37 @@ pytest_plugins: list[str] = ["pytester"]
 
 
 @pytest.fixture(scope="session")
-def robust_python_demo_path(tmp_path_factory: TempPathFactory) -> Path:
+def demos_folder(tmp_path_factory: TempPathFactory) -> Path:
+    """Temp Folder used for storing demos while testing."""
+    return tmp_path_factory.mktemp("demos")
+
+
+@pytest.fixture(scope="session")
+def robust_python_demo_path(demos_folder: Path) -> Path:
     """Creates a temporary example python project for testing against and returns its Path."""
-    demos_path: Path = tmp_path_factory.mktemp("demos")
     cookiecutter(
         str(REPO_FOLDER),
         no_input=True,
         overwrite_if_exists=True,
-        output_dir=demos_path,
+        output_dir=demos_folder,
         extra_context={"project_name": "robust-python-demo", "add_rust_extension": False},
     )
-    path: Path = demos_path / "robust-python-demo"
-    subprocess.run(["uv", "lock"], cwd=path)
+    path: Path = demos_folder / "robust-python-demo"
+    subprocess.run(["nox", "-s", "setup-repo"], cwd=path, capture_output=True)
     return path
 
 
 @pytest.fixture(scope="session")
-def robust_maturin_demo_path(tmp_path_factory: TempPathFactory) -> Path:
+def robust_maturin_demo_path(demos_folder: Path) -> Path:
     """Creates a temporary example maturin project for testing against and returns its Path."""
-    demos_path: Path = tmp_path_factory.mktemp("demos")
     cookiecutter(
         str(REPO_FOLDER),
         no_input=True,
         overwrite_if_exists=True,
-        output_dir=demos_path,
-        extra_context={"project_name": "robust-maturin-demo", "add_rust_extension": True},
+        output_dir=demos_folder,
+        extra_context={"project_name": "robust-maturin-demo", "add_rust_extension": True}
     )
-    path: Path = demos_path / "robust-maturin-demo"
-    subprocess.run(["uv", "sync"], cwd=path)
+    path: Path = demos_folder / "robust-maturin-demo"
+    subprocess.run(["nox", "-s", "setup-repo"], cwd=path, capture_output=True)
     return path
 
-
-@pytest.fixture(scope="function")
-def inside_robust_python_demo(robust_python_demo_path: Path) -> Generator[Path, None, None]:
-    """Changes the current working directory to the robust-python-demo project."""
-    original_path: Path = Path.cwd()
-    os.chdir(robust_python_demo_path)
-    yield robust_python_demo_path
-    os.chdir(original_path)
-
-
-@pytest.fixture(scope="function")
-def inside_robust_maturin_demo(robust_maturin_demo_path: Path) -> Generator[Path, None, None]:
-    original_path: Path = Path.cwd()
-    os.chdir(robust_maturin_demo_path)
-    yield robust_maturin_demo_path
-    os.chdir(original_path)
--- tests/integration_tests/test_robust_python_demo.py
+++ tests/integration_tests/test_robust_python_demo.py
@@ -13,15 +13,48 @@ def test_demo_project_generation(robust_python_demo_path: Path) -> None:
 
 
 @pytest.mark.parametrize("session", GLOBAL_NOX_SESSIONS)
-def test_demo_project_noxfile(robust_python_demo_path: Path, session: str) -> None:
-    command: list[str] = ["uvx", "nox", "-s", session]
+def test_demo_project_nox_session(robust_python_demo_path: Path, session: str) -> None:
+    command: list[str] = ["nox", "-s", session]
     result: subprocess.CompletedProcess = subprocess.run(
         command,
         cwd=robust_python_demo_path,
         capture_output=True,
         text=True,
-        timeout=10.0,
+        timeout=20.0
     )
     print(result.stdout)
     print(result.stderr)
     result.check_returncode()
+
+
+def test_demo_project_nox_pre_commit(robust_python_demo_path: Path) -> None:
+    command: list[str] = ["nox", "-s", "pre-commit"]
+    result: subprocess.CompletedProcess = subprocess.run(
+        command,
+        cwd=robust_python_demo_path,
+        capture_output=True,
+        text=True,
+        timeout=20.0
+    )
+    assert result.returncode == 0
+
+
+def test_demo_project_nox_pre_commit_with_install(robust_python_demo_path: Path) -> None:
+    command: list[str] = ["nox", "-s", "pre-commit", "--", "install"]
+    pre_commit_hook_path: Path = robust_python_demo_path / ".git" / "hooks" / "pre-commit"
+    assert not pre_commit_hook_path.exists()
+
+    result: subprocess.CompletedProcess = subprocess.run(
+        command,
+        cwd=robust_python_demo_path,
+        capture_output=True,
+        text=True,
+        timeout=20.0
+    )
+    assert pre_commit_hook_path.exists()
+    assert pre_commit_hook_path.is_file()
+
+    assert result.returncode == 0
+
+
+
