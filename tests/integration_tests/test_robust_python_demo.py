423a5a4bcb081ab0fcb1b0f722861bddc3b45a81
Kyle Oliver <oliverke@mail.uc.edu>
2025-06-09 23:48:41 -0400

    fix: adjust forgotten names in fixtures

--- tests/integration_tests/test_robust_python_demo.py
+++ tests/integration_tests/test_robust_python_demo.py
@@ -8,27 +8,27 @@ import pytest
 from tests.constants import GLOBAL_NOX_SESSIONS
 
 
-def test_demo_project_generation(robust_python_demo: Path) -> None:
-    assert robust_python_demo.exists()
+def test_demo_project_generation(robust_python_demo_with_setup: Path) -> None:
+    assert robust_python_demo_with_setup.exists()
 
 
 @pytest.mark.parametrize("session", GLOBAL_NOX_SESSIONS)
-def test_demo_project_nox_session(robust_python_demo: Path, session: str) -> None:
+def test_demo_project_nox_session(robust_python_demo_with_setup: Path, session: str) -> None:
     command: list[str] = ["nox", "-s", session]
     result: subprocess.CompletedProcess = subprocess.run(
         command,
-        cwd=robust_python_demo,
+        cwd=robust_python_demo_with_setup,
         capture_output=True,
     )
     if result.check_returncode() != 0:
         raise SystemExit(result.stdout)
 
 
-def test_demo_project_nox_pre_commit(robust_python_demo: Path) -> None:
+def test_demo_project_nox_pre_commit(robust_python_demo_with_setup: Path) -> None:
     command: list[str] = ["nox", "-s", "pre-commit"]
     result: subprocess.CompletedProcess = subprocess.run(
         command,
-        cwd=robust_python_demo,
+        cwd=robust_python_demo_with_setup,
         capture_output=True,
         text=True,
         timeout=20.0
@@ -36,14 +36,14 @@ def test_demo_project_nox_pre_commit(robust_python_demo: Path) -> None:
     assert result.returncode == 0
 
 
-def test_demo_project_nox_pre_commit_with_install(robust_python_demo_no_setup: Path) -> None:
+def test_demo_project_nox_pre_commit_with_install(robust_python_demo_with_setup: Path) -> None:
     command: list[str] = ["nox", "-s", "pre-commit", "--", "install"]
-    pre_commit_hook_path: Path = robust_python_demo / ".git" / "hooks" / "pre-commit"
+    pre_commit_hook_path: Path = robust_python_demo_with_setup / ".git" / "hooks" / "pre-commit"
     assert not pre_commit_hook_path.exists()
 
     result: subprocess.CompletedProcess = subprocess.run(
         command,
-        cwd=robust_python_demo,
+        cwd=robust_python_demo_with_setup,
         capture_output=True,
         text=True,
         timeout=20.0
