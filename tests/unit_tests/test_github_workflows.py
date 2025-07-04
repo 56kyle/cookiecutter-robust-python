941a5df4d7583bd3d7284546846d96a1a9b7709c
Kyle Oliver <oliverke@mail.uc.edu>
2025-07-03 21:47:34 -0400

    refactor: rearrange parametrization order in test_github_workflows.py to better display test values

--- tests/unit_tests/test_github_workflows.py
+++ tests/unit_tests/test_github_workflows.py
@@ -7,23 +7,23 @@ from util import templates_matching
 
 
 @pytest.mark.parametrize(
+    argnames="robust_demo__is_setup",
+    argvalues=[False],
+    indirect=True,
+    ids=["no-setup"]
+)
+@pytest.mark.parametrize(
     argnames="robust_demo__add_rust_extension",
     argvalues=[False, True],
     indirect=True,
     ids=["base", "maturin"]
 )
 @pytest.mark.parametrize(
-    argnames="robust_demo__is_setup",
-    argvalues=[False],
+    argnames="robust_file__path__relative",
+    argvalues=templates_matching(".github/workflows/*.yml"),
     indirect=True,
-    ids=["no-setup"]
+    ids=lambda path: path.stem
 )
 class TestWorkflow:
-    @pytest.mark.parametrize(
-        argnames="robust_file__path__relative",
-        argvalues=templates_matching(".github/workflows/*.yml"),
-        indirect=True,
-        ids=lambda path: path.name
-    )
     def test_workflow_basic_loading(self, robust_yaml: dict[str, Any]) -> None:
         assert robust_yaml
