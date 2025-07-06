5f97c0ca415c45a71e20160cce8ac5fd4eb9817e
Kyle Oliver <oliverke@mail.uc.edu>
2025-07-05 20:16:43 -0400

    fix: add missing dependencies for the update-demo nox session

--- noxfile.py
+++ noxfile.py
@@ -125,6 +125,9 @@ def test(session: Session) -> None:
 @nox.parametrize(arg_names="add_rust_extension", arg_values_list=[False, True], ids=["no-rust", "with-rust"])
 @nox.session(name="update-demo", python=DEFAULT_TEMPLATE_PYTHON_VERSION)
 def update_demo(session: Session, add_rust_extension: bool) -> None:
+    session.log("Installing script dependencies for updating generated project demos...")
+    session.install("cookiecutter", "cruft", "platformdirs", "loguru", "typer")
+
     session.log("Updating generated project demos...")
     args: list[str] = [*UPDATE_DEMO_OPTIONS]
     if add_rust_extension:
