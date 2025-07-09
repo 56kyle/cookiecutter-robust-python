a77aa4d78cc34e55b4a187437d067ccd14657730
Kyle Oliver <oliverke@mail.uc.edu>
2025-07-08 22:26:49 -0400

    fix: remove type alias to avoid needing typing_extensions

--- {{cookiecutter.project_name}}/scripts/util.py
+++ {{cookiecutter.project_name}}/scripts/util.py
@@ -6,14 +6,10 @@ import subprocess
 from pathlib import Path
 from typing import Any
 from typing import Callable
-from typing import Literal
 from typing import Optional
-from typing_extensions import TypeAlias
 
 
 REPO_FOLDER: Path = Path(__file__).resolve().parent.parent
-
-Increment: TypeAlias = Literal["MAJOR", "MINOR", "PATCH", "PRERELEASE"]
 
 
 class MissingDependencyError(Exception):
@@ -69,7 +65,7 @@ def get_package_version() -> str:
     return result.stdout.decode("utf-8").strip()
 
 
-def get_bumped_package_version(increment: Optional[Increment] = None) -> str:
+def get_bumped_package_version(increment: Optional[str] = None) -> str:
     """Gets the bumped package version."""
     args: list[str] = ["uvx", "cz", "bump", "--get-next", "--yes", "--dry-run"]
     if increment is not None:
@@ -89,7 +85,7 @@ def create_release_branch(new_version: str) -> None:
         subprocess.run(command, cwd=REPO_FOLDER, capture_output=True, check=True)
 
 
-def bump_version(increment: Optional[Increment] = None) -> None:
+def bump_version(increment: Optional[str] = None) -> None:
     """Bumps the package version."""
     bump_cmd: list[str] = ["uvx", "cz", "bump", "--yes", "--files-only", "--changelog"]
     if increment is not None:
