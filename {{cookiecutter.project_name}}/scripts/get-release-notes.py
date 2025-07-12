471482a0309f5767365c99ec5723fb4983aa1b74
Kyle Oliver <oliverke@mail.uc.edu>
2025-07-12 18:17:15 -0400

    feat: revert back to writing out a body.md file

--- {{cookiecutter.project_name}}/scripts/get-release-notes.py
+++ {{cookiecutter.project_name}}/scripts/get-release-notes.py
@@ -9,7 +9,9 @@ RELEASE_NOTES_PATH: Path = Path("body.md")
 
 def main() -> None:
     """Parses args and passes through to bump_version."""
-    get_latest_release_notes()
+    release_notes: str = get_latest_release_notes()
+    RELEASE_NOTES_PATH.write_text(release_notes)
+
 
 
 if __name__ == "__main__":
